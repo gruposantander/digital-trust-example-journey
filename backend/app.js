24996b430254bb71d96a4da8b7ed2ebcc4144a3e
Pulido Moyano, Alberto (SanTech) <alberto.pulido@santander.co.uk>
2020-09-24 12:09:23 +0100

    Updated README with links to DTP docs

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const { Claims, AssertionClaims, Address } = require('@gruposantander/rp-client-typescript').Model
+const { Claims, AssertionClaims, Address, Balance} = require('@gruposantander/rp-client-typescript').Model
 const { VerifiedIdClient, InitiateAuthorizeRequestBuilder, TokenRequestBuilder } = require('@gruposantander/rp-client-typescript').Client
 const Joi = require('@hapi/joi');
 const resolve = require('path').resolve;
@@ -12,7 +12,7 @@ const wellKnown = process.env.WELL_KNOWN_URL || 'https://live.iamid.io/.well-kno
 const clientId = process.env.CLIENT_ID || 'Ds2UChhNmck7Jcakyxvgi';
 const redirectUri = process.env.REDIRECT_URI ||  'http://localhost:4201/profile';
 const staticsFolderRelativePath = process.env.STATICS_FOLDER || '/../dist/digital-id-example-frontend';
-const cronSchedule = process.env.CRON_RESET_SCHEDULE || '*/1 * * * *';
+const cronSchedule = process.env.CRON_RESET_SCHEDULE || '*/10 * * * *';
 
 let verified = false;
 let resetScheduler = schedule.scheduleJob(cronSchedule, function(){
@@ -21,29 +21,31 @@ let resetScheduler = schedule.scheduleJob(cronSchedule, function(){
   });
 
 let defaultUserDetails = {
-    title: "Mrs",
-    given_name: "Laura",
-    family_name: "Lavine",
-    country_of_birth: "GB",
-    address: {
-        street_address: '91, Savannah Falls',
-        locality: 'Rotherham',
-        postal_code: 'CE0YYW',
-        country: 'United Kingdom',
+    "title": "Mrs",
+    "given_name": "Yost",
+    "family_name": "Hilton",
+    "country_of_birth": "GB",
+    "address": {
+        "street_address": "19 Kacey Forest",
+        "locality": "Redding",
+        "postal_code": "QZBAD9",
+        "country": "United Kingdom"
     }
 }
-let userDetails = {
-    title: "Mrs",
-    given_name: "Laura",
-    family_name: "Lavine",
-    country_of_birth: "GB",
-    address: {
-        street_address: '91, Savannah Falls',
-        locality: 'Rotherham',
-        postal_code: 'CE0YYW',
-        country: 'United Kingdom',
+let userDetails = 
+    {
+        "title": "Mrs",
+        "given_name": "Yost",
+        "family_name": "Hilton",
+        "country_of_birth": "GB",
+        "address": {
+            "street_address": "19 Kacey Forest",
+            "locality": "Redding",
+            "postal_code": "QZBAD9",
+            "country": "United Kingdom"
+        }
     }
-}
+
 
 app.use( express.static( __dirname + staticsFolderRelativePath))
 app.use('/profile', express.static( __dirname + staticsFolderRelativePath))
@@ -86,7 +88,10 @@ app.get('/initiate-authorize', async (req, res) => {
         .withAssertion(Address.postalCode().eq(userDetails.address.postal_code))
         .withAssertion(Address.country().eq(userDetails.address.country))
         .withPurpose('We want to verify your address is correct, so that we can address any correspondence with you, and between yourself and any future employers correctly in a professional manner. We also need to ensure that you have a place of residence within the UK.')
-
+    // assertionClaims.lastYearMoneyIn()
+    //     .withAssertion(Balance.currency().eq("GBP"))
+    //     .withAssertion(Balance.amount().gt(30000.00))
+    //     .withIAL(3)
     let verifyidclient;
     try {
         verifyidclient = await VerifiedIdClient.createInstance({
